3046b4953ed552a3662278186a95e5e2c3a8674b
Irvan Kadhafi <irvan.kadhafi.tif18@polban.ac.id>
2020-04-23 22:36:37 +0700

    Homework Updated

--- app/src/main/java/com/irvankdhf/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/irvankdhf/simpleasynctask/MainActivity.java
@@ -4,25 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
-    private TextView mProgressTextView;
     private static final String TEXT_STATE = "currentText";
-    private static final String TEXT_PROGRESS = "CurrentSleepDuration";
+//    private TextView mProgressTextView;
+//    private static final String TEXT_PROGRESS = "CurrentSleepDuration";
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textView1);
-        mProgressTextView = findViewById(R.id.textViewResult);
+//        mProgressTextView = findViewById(R.id.textViewResult);
+        mProgressBar = findViewById(R.id.progressBar);
 
         if(savedInstanceState != null) {
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
-            mProgressTextView.setText(savedInstanceState.getString(TEXT_PROGRESS));
+//            mProgressTextView.setText(savedInstanceState.getString(TEXT_PROGRESS));
         }
     }
 
@@ -30,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(TEXT_STATE, mTextView.getText().toString());
-        outState.putString(TEXT_PROGRESS, mProgressTextView.getText().toString());
+//        outState.putString(TEXT_PROGRESS, mProgressTextView.getText().toString());
     }
 
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
-        new SimpleAsyncTask(mTextView,mProgressTextView).execute();
+        new SimpleAsyncTask(mTextView,mProgressBar).execute();
     }
 }
--- app/src/main/java/com/irvankdhf/simpleasynctask/SimpleAsyncTask.java
+++ app/src/main/java/com/irvankdhf/simpleasynctask/SimpleAsyncTask.java
@@ -1,6 +1,7 @@
 package com.irvankdhf.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -9,33 +10,40 @@ import java.util.Random;
 public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
 
     private WeakReference<TextView> mTextView;
-    private WeakReference<TextView> mResultTextView;
+    //    private WeakReference<TextView> mResultTextView;
+    private WeakReference<ProgressBar> mProgressBar;
+    private static final int CHUNK_SIZE = 5;
 
-    public SimpleAsyncTask(TextView tv, TextView result) {
+    public SimpleAsyncTask(TextView tv, ProgressBar bar) {
         mTextView = new WeakReference<>(tv);
-        mResultTextView = new WeakReference<>(result);
+//        mResultTextView = new WeakReference<>(result);
+        mProgressBar = new WeakReference<>(bar);
     }
 
     @Override
     protected String doInBackground(Void... voids) {
         Random r = new Random();
         int n = r.nextInt(11);
-
         int s = n * 200;
-        publishProgress(n);
 
-        try {
-            Thread.sleep(s);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        int chunkSize = s / CHUNK_SIZE;
+
+//        publishProgress(n);
+        for (int i = 0; i < CHUNK_SIZE; i++) {
+            try {
+                Thread.sleep(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            publishProgress(((i + 1) * 100) / CHUNK_SIZE);
         }
-
         return "Awake at last after sleep " + s + " milliseconds!";
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        mResultTextView.get().setText("Current sleep time: " + values[0] + " ms");
+//        mResultTextView.get().setText("Current sleep time: " + values[0] + " ms");
+          mProgressBar.get().setProgress(values[0]);
     }
 
     @Override
